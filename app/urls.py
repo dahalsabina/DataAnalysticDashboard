7fc61a0bcac58f6e435c02db423f8a8dd52fd788
dahalSabina <dahasa01@luther.edu>
2024-06-28 22:27:01 -0500

    Rendered different charts

---
+++ app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+     path ("area-chart", views.area_chart, name= "app-area-chart"),
+     path("", views.index, name = "app-index")
+   
+]
